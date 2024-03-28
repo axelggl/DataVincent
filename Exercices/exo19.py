15ec311e643865ff04740bd607f528bdb125856c
maxel <axel.maerten@ynov.com>
2024-03-28 17:43:55 +0100

    Exercice 19

---
+++ Exercices/exo19.py
@@ -0,0 +1,7 @@
+# Écrivez une condition qui vérifie si une variable `x` est supérieure à 10.
+
+x = 11
+if x > 10:
+    print("x est supérieur à 10")
+else:
+    print("x est inférieur ou égal à 10")
