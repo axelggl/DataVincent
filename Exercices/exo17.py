3eadc744e5054a9829f48226f83cb41d4570d829
maxel <axel.maerten@ynov.com>
2024-03-28 17:42:10 +0100

    Exercice 17

---
+++ Exercices/exo17.py
@@ -0,0 +1,4 @@
+# Créez un tuple contenant les mois de l'année.
+
+mois = ("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre")
+print(mois) # Affiche le tuple contenant les mois de l'année
