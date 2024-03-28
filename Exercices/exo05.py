a41c215a89e299b883bb9f894a1723ac45363a81
maxel <axel.maerten@ynov.com>
2024-03-28 17:03:22 +0100

    Exercice 05

---
+++ Exercices/exo05.py
@@ -0,0 +1,11 @@
+# Comment pouvez-vous vérifier le type d'une variable?
+
+nombre = 42
+texte = "42"
+tableau = [1, 2, 3, 4]
+boolean = True
+
+print(type(nombre)) # Affiche le type de nombre, ici int
+print(type(texte)) # Affiche le type de texte, ici str
+print(type(tableau)) # Affiche le type de tableau, ici list
+print(type(boolean)) # Affiche le type de boolean, ici bool
