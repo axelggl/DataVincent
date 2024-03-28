9393e69dfeeac209a9dcfe209b6296cb05d9d935
maxel <axel.maerten@ynov.com>
2024-03-28 17:01:04 +0100

    Exercice 04

---
+++ Exercices/exo04.py
@@ -0,0 +1,7 @@
+# Comment convertissez-vous la chaîne de caractères `"1234"` en un nombre entier?
+
+texte = "1234"
+nombre = int(texte)
+
+print(nombre) # Affiche 1234 en tant qu'int
+print(type(nombre)) # Affiche le type de nombre, ici int
