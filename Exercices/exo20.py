e897c22e2438ba886850fa056fc7e96862aa94df
maxel <axel.maerten@ynov.com>
2024-03-28 17:45:09 +0100

    Exercice 20

---
+++ Exercices/exo20.py
@@ -0,0 +1,6 @@
+# Utilisez une boucle `for` pour afficher chaque jour de la semaine.
+
+jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+
+for jour in jours:
+    print(jour) # Affiche chaque jour de la semaine
