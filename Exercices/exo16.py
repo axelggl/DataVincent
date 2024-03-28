eba1c148543858080372499c328cf17e7b237959
maxel <axel.maerten@ynov.com>
2024-03-28 17:40:22 +0100

    Exercice 16

---
+++ Exercices/exo16.py
@@ -0,0 +1,3 @@
+# Quelle est la différence entre les tuples et les listes?
+
+# Réponse : les tuples ne peuvent pas être modifiés après avoir été créés, alors que les listes peuvent être modifiées. De plus, les tuples sont créés en utilisant des parenthèses, là où les listes sont créées avec des crochets.
