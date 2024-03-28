8e3bb0420021f8897684a7982443a13e6aaca681
maxel <axel.maerten@ynov.com>
2024-03-28 17:34:38 +0100

    Exercices 11 - 13

---
+++ Exercices/exo11-13.py
@@ -0,0 +1,17 @@
+# Exercice 11 : Créez un dictionnaire représentant un livre avec les clés: titre, auteur, et année
+
+livre = {
+    "titre": "1981",
+    "auteur": "George Orwell",
+    "année": 2025
+}
+print(livre) # Affiche le dictionnaire représentant le livre
+
+# Exercice 12 : Ajoutez une clé `pages` avec un nombre entier représentant le nombre de pages du livre.
+
+livre["pages"] = 727
+print(livre) # Affiche le dictionnaire représentant le livre
+
+# Exercice 13 : Afficher seulement les auteurs du dictionnaire
+
+print(livre["auteur"]) # Affiche l'auteur du livre
