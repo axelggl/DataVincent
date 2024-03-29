ea4b1b96542c2da6b392fb3fdedf561d8199079f
maxel <axel.maerten@ynov.com>
2024-03-29 09:29:30 +0100

    Exercice 21

---
+++ Exercices/Partie 2/exo21.py
@@ -0,0 +1,7 @@
+# Écrivez une boucle `while` qui affiche les nombres de 1 à 10.
+
+# Cette boucle permet d'afficher les nombres de 1 à 10
+i = 1 # On initialise la variable i à 1
+while i <= 10: # Tant que i est inférieur ou égal à 10, on rentre dans la boucle.
+    print(i)
+    i += 1
