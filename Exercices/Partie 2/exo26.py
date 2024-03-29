136a44b4dad1f84cf783358c43c8351535a765a9
maxel <axel.maerten@ynov.com>
2024-03-29 10:22:51 +0100

    Exercice 26

---
+++ Exercices/Partie 2/exo26.py
@@ -0,0 +1,5 @@
+# Utilisez une compréhension de liste pour créer une liste des carrés des nombres de 1 à 10
+
+nombres = [i ** 2 for i in range(1, 11)]
+
+print(nombres) # Affiche la liste des carrés des nombres de 1 à 10
