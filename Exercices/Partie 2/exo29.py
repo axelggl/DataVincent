8ea9be9238f16e3d355e084c1681db8d5a22f7ef
maxel <axel.maerten@ynov.com>
2024-03-29 10:30:33 +0100

    Exercice 29

---
+++ Exercices/Partie 2/exo29.py
@@ -0,0 +1,11 @@
+# Créez un dictionnaire de livres où chaque clé est le titre du livre, et la valeur est l'auteur. Accédez à l'auteur d'un livre spécifique.
+
+livres = {
+    "Livre 1": "John Doe",
+    "Livre 2": "Jean Dupont",
+    "Livre 3": "Frédéric-Jacques de la Rivière"
+}
+
+print(livres["Livre 1"]) # John Doe
+print(livres["Livre 2"]) # Jean Dupont
+print(livres["Livre 3"]) # Frédéric-Jacques de la Rivière
