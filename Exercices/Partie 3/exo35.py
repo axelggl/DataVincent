5234a6691f998685f841d52a71d53f1cfa815b2c
maxel <axel.maerten@ynov.com>
2024-03-29 10:49:21 +0100

    Exercice 35

---
+++ Exercices/Partie 3/exo35.py
@@ -0,0 +1,4 @@
+# Installez le package `numpy` en utilisant pip via la ligne de commande (hors de Python).and
+
+# Installer pip : https://pip.pypa.io/en/stable/installation/
+# Installer numpy : https://numpy.org/install/
