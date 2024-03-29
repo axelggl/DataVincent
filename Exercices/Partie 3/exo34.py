799bde163176e39a3babb7a0ae2efb33a01f0634
maxel <axel.maerten@ynov.com>
2024-03-29 10:43:35 +0100

    Exercice 34

---
+++ Exercices/Partie 3/exo34.py
@@ -0,0 +1,3 @@
+# Quelle est la différence entre `import nom_module` et `from nom_module import fonction`?
+
+# import nom_module importe tout le module, alors que from nom_module import fonction importe seulement la fonction du module.
